ccad26a3a0a9743f7ea3267920d16ddf3c5b0929
OP-Devloop <oscar.pettersson@iths.se>
2026-05-19 10:01:57 +0200

    Initial commit

---
+++ src/main/java/se/iths/oscarp/microprojectauthserver/MicroProjectAuthServerApplication.java
@@ -0,0 +1,13 @@
+package se.iths.oscarp.microprojectauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroProjectAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroProjectAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/iths/oscarp/microprojectauthserver/MicroProjectAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.oscarp.microprojectauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroProjectAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
