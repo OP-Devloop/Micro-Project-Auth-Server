14e4504941c0a7de7590706a864426a55ba07ee3
OP-Devloop <oscar.pettersson@iths.se>
2026-05-19 10:31:11 +0200

    Add initial controller, service, mapper, DTO, and configuration classes

---
+++ src/main/java/se/iths/oscarp/microprojectauthserver/controller/AppUserController.java
@@ -0,0 +1,4 @@
+package se.iths.oscarp.microprojectauthserver.controller;
+
+public class AppUserController {
+}
---
+++ src/main/java/se/iths/oscarp/microprojectauthserver/mapper/AppUserMapper.java
@@ -0,0 +1,4 @@
+package se.iths.oscarp.microprojectauthserver.mapper;
+
+public class AppUserMapper {
+}
