b1859053628d60e17de38502b9e17dd2dfa395c5
OP-Devloop <oscar.pettersson@iths.se>
2026-05-19 10:45:43 +0200

    Rename 'name' field to 'username' in AppUser entity

--- src/main/java/se/iths/oscarp/microprojectauthserver/model/AppUser.java
+++ src/main/java/se/iths/oscarp/microprojectauthserver/model/AppUser.java
@@ -17,7 +17,7 @@ public class AppUser {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    private String username;
     private LocalDate dateOfBirth;
     private String createdBy;
 }
