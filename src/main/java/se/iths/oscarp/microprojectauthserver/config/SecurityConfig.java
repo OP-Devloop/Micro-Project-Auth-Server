91d43eac95e83a8c4f2c05d4fcd15f22b014dc17
OP-Devloop <oscar.pettersson@iths.se>
2026-05-20 11:34:11 +0200

    Add security configuration for JWT authentication and authorization rules

--- src/main/java/se/iths/oscarp/microprojectauthserver/config/SecurityConfig.java
+++ src/main/java/se/iths/oscarp/microprojectauthserver/config/SecurityConfig.java
@@ -1,4 +1,39 @@
 package se.iths.oscarp.microprojectauthserver.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   JwtAuthenticationConverter jwtAuthenticationConverter
+    ) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/error").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/appusers/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/appusers").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/appusers/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt ->
+                        jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+        return http.build();
+    }
 }
