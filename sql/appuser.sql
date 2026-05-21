028db63319b86c89f923e2e89c7038c36f218b3a
OP-Devloop <oscar.pettersson@iths.se>
2026-05-21 09:46:54 +0200

    Add password field to AppUser and create app_user table in SQL

---
+++ sql/appuser.sql
@@ -0,0 +1,9 @@
+create table app_user
+(
+    id            bigserial primary key,
+    username      varchar(255) NOT NULL,
+    password      varchar(255) NOT NULL,
+    role          varchar(255) NOT NULL,
+    date_of_birth date,
+    created_by    varchar(255)
+);
