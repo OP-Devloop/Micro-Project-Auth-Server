c461b2f381045869e8254b06f2a1661e2ab717ad
OP-Devloop <oscar.pettersson@iths.se>
2026-05-26 08:47:07 +0200

    Add KeyGenMain class for RSA key pair generation

---
+++ src/main/java/se/iths/oscarp/microprojectauthserver/KeyGenMain.java
@@ -0,0 +1,21 @@
+package se.iths.oscarp.microprojectauthserver;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.util.Base64;
+
+public class KeyGenMain {
+    public static void main(String[] args) throws Exception {
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+        generator.initialize(2048);
+        KeyPair keyPair = generator.generateKeyPair();
+        String privateKey = Base64.getEncoder()
+                .encodeToString(keyPair.getPrivate().getEncoded());
+        String publicKey = Base64.getEncoder()
+                .encodeToString(keyPair.getPublic().getEncoded());
+        System.out.println("Private key att kopiera:");
+        System.out.println(privateKey);
+        System.out.println("Public key att kopiera:");
+        System.out.println(publicKey);
+    }
+}
